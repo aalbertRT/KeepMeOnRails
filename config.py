d27383015746cbc520a0bfa999507c14280091e9
aurelienalbert <aurelienalbert.dev@gmail.com>
2021-03-24 21:30:54 +0100

    [+] Add db models and sqlalchemy dependency

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-SNCF_TOKEN = ""
+import os
+
+# Database
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+# Credentials
+SNCF_TOKEN = ''
