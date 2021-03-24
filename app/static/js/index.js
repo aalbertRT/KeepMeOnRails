d8a2f69262330317666f9026241f002d6171b904
aurelienalbert <aurelienalbert.dev@gmail.com>
2021-03-24 07:58:38 +0100

    [+] Add POST method for the user form

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -14,7 +14,13 @@ function onSubmit(event) {
 	//form.reset();
 
 	var request = new XMLHttpRequest();
-	request.open("POST", "localhost:8080");
+	request.onreadystatechange = function() {
+		if (request.readyState == XMLHttpRequest.DONE && request.status == 200) {
+			var response = request.responseText
+			console.log(response);
+		}
+	}
+	request.open("POST", "/ajax/");
 	request.setRequestHeader("Content-type", "application/json");
 	request.send(JSON.stringify(formInput));
 }
