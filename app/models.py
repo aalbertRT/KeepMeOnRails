c606566a801ab4b361ce9f170127954f94e35b7b
aurelienalbert <aurelienalbert.dev@gmail.com>
2021-03-24 21:43:41 +0100

    [~] Clean bug code

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(200), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)User
+    email = db.Column(db.String(120), unique=True, nullable=False)
     phone_number = db.Column(db.String(200), unique=True, nullable=False)
     # Relationship: a user can have multiple planned trips
     # It is also mandatory to add property 'user' to the Trip object 
