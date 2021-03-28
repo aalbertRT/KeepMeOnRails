eaaa59a59ba8c8f31d3079c2cc376a479c0d55ba
aurelienalbert <aurelienalbert.dev@gmail.com>
2021-03-28 18:06:06 +0200

    [-] Purge

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Database
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-
-# Credentials
-SNCF_TOKEN = ''
