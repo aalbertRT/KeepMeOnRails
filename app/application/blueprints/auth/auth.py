ec87d9db4699bb17265f6a5f06719181cbc962c3
aurelienalbert <aurelienalbert.dev@gmail.com>
2021-04-15 21:10:59 +0200

    [+] Add dashboard, profile

--- app/application/blueprints/auth/auth.py
+++ app/application/blueprints/auth/auth.py
@@ -1,29 +1,28 @@
+"""Authentication blueprint."""
+
 from datetime import datetime
 
-from flask import Blueprint, render_template, redirect, request, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import current_user, login_user
 
-from application.models import User
 from application.interface import UserInterface
 from application.service import UserService
 from application.forms import LoginForm, SignupForm
 from application import login_manager
 
 # Blueprint configuration
-auth_bp = Blueprint('auth_bp', __name__)
+auth_bp = Blueprint("auth_bp", __name__)
 
-@auth_bp.route('/login/', methods=['GET', 'POST'])
+
+@auth_bp.route("/login/", methods=["GET", "POST"])
 def login():
     """Log in form page."""
     # Bypass if user logged in
     if current_user.is_authenticated:
-        return redirect(url_for('home_bp.index'))
+        return redirect(url_for("home_bp.index"))
 
     form = LoginForm()
-    variables = {
-        'title': 'Keep me on rails - Login',
-        'form': form
-    }
+    variables = {"title": "Keep me on rails - Login", "form": form}
     if form.validate_on_submit():
         user = UserService.get_by_email(form.email.data)
         # Check the user exists
@@ -33,36 +32,33 @@ def login():
                 # Log in the user
                 login_user(user)
                 UserService.update_last_login(user, datetime.now())
-                return redirect(url_for('home_bp.index'))
-            flash('Invalid user/password combination.')
-            return render_template('login.html', **variables)
-        flash ('This email is not registered, please signup.')
-        return redirect(url_for('auth_bp.signup'))
-    return render_template('login.html', **variables)
+                return redirect(url_for("home_bp.index"))
+            flash("Invalid user/password combination.")
+            return render_template("login.html", **variables)
+        flash("This email is not registered, please signup.")
+        return redirect(url_for("auth_bp.signup"))
+    return render_template("login.html", **variables)
 
-@auth_bp.route('/signup/', methods=['GET', 'POST'])
+
+@auth_bp.route("/signup/", methods=["GET", "POST"])
 def signup():
     """Signup form page."""
     form = SignupForm()
-    variables = {
-        'title': 'Keep me on rails - Signup',
-        'form': form
-    }
+    variables = {"title": "Keep me on rails - Signup", "form": form}
     if form.validate_on_submit():
         user_interface = UserInterface(
-            email = form.email.data,
-            username = form.name.data,
-            password = form.password.data
+            email=form.email.data, username=form.name.data, password=form.password.data
         )
         # Check email is not already used by an existing user
-        if not UserService.get_by_email(user_interface['email']):
+        if not UserService.get_by_email(user_interface["email"]):
             new_user = UserService.create(user_interface)
             # Log in as the new user
             login_user(new_user)
             UserService.update_last_login(new_user, datetime.now())
-            return redirect(url_for('home_bp.index'))
-        flash('A user already exists with that email address.')
-    return render_template('signup.html', **variables)
+            return redirect(url_for("home_bp.dashboard"))
+        flash("A user already exists with that email address.")
+    return render_template("signup.html", **variables)
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -71,8 +67,9 @@ def load_user(user_id):
         return UserService.get_by_id(user_id)
     return None
 
+
 @login_manager.unauthorized_handler
 def unauthorized():
     """Redirect unauthorized user to Login page form."""
-    flash('You muste be logged in to be on this page.')
-    return redirect(url_for('auth_bp.login'))
+    flash("You muste be logged in to be on this page.")
+    return redirect(url_for("auth_bp.login"))
--- app/application/blueprints/home/home.py
+++ app/application/blueprints/home/home.py
@@ -1,57 +1,76 @@
-import json
+"""Home blueprint."""
 
-from flask import Blueprint, flash, render_template, request, url_for, make_response, jsonify, redirect
+from flask import (
+    Blueprint,
+    flash,
+    render_template,
+    url_for,
+    redirect,
+)
 from flask import current_app as app
 from flask_login import login_required, logout_user, current_user
 
-from application.models import User, Trip
-from application.interface import UserInterface, TripInterface
-from application.service import UserService, TripService
+from application.models import Trip
+from application.interface import TripInterface
+from application.service import TripService
 from application.forms import TripForm
 
 # Blueprint configuration
-home_bp = Blueprint('home_bp', __name__)
+home_bp = Blueprint("home_bp", __name__)
 
-@home_bp.route('/index/', methods=['GET', 'POST'])
+
+@home_bp.route("/index/", methods=["GET", "POST"])
 @login_required
 def index():
     """Index page."""
     form = TripForm()
     variables = {
-        'title': 'Keep me on rails - Trip form',
-        'sncf_token': app.config['SNCF_TOKEN'],
-        'form': form
+        "title": "Keep me on rails - Trip form",
+        "sncf_token": app.config["SNCF_TOKEN"],
+        "form": form,
     }
     if form.validate_on_submit():
-        print('valid form')
         # Parse trip data and test existence at this date for the user
         trip_interface = TripInterface(
             user_id=current_user.id,
             departure=form.departure.data,
             arrival=form.arrival.data,
-            date=form.date.data
+            date=form.date.data,
         )
         # Verify existence in db
-        result = Trip.query.filter(
-            Trip.user_id==trip_interface['user_id']
-        ).filter(
-            Trip.date==trip_interface['date']
-        ).filter(
-            Trip.departure==trip_interface['departure']
-        ).filter(
-            Trip.arrival==trip_interface['arrival']
-        ).first()
+        result = (
+            Trip.query.filter(Trip.user_id == trip_interface["user_id"])
+            .filter(Trip.date == trip_interface["date"])
+            .filter(Trip.departure == trip_interface["departure"])
+            .filter(Trip.arrival == trip_interface["arrival"])
+            .first()
+        )
         if result is None:
             TripService.create(trip_interface)
-            flash('Trip request created.')
+            flash("Trip request created.")
         else:
-            flash('Trip request already exists.')
-        return redirect(url_for('home_bp.index'))
-    return render_template('trip_form.html', **variables)
+            flash("Trip request already exists.")
+        return redirect(url_for("home_bp.index"))
+    return render_template("trip_form.html", **variables)
 
-@home_bp.route('/logout/')
+
+@home_bp.route("/profile/", methods=["GET"])
+@login_required
+def profile():
+    """Profile page."""
+    return render_template("profile.html", user=current_user)
+
+
+@home_bp.route("/dashboard/", methods=["GET"])
+@login_required
+def dashboard():
+    """Dashboard page."""
+    return render_template("dashboard.html", trips=current_user.trips)
+
+
+@home_bp.route("/logout/")
 @login_required
 def logout():
     """User log out logic."""
     logout_user()
-    return redirect(url_for('auth_bp.login'))
+    return redirect(url_for("auth_bp.login"))
